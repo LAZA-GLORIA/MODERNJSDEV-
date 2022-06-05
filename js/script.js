e636cb29ea3d1cde2a8c38cab44910f2cca758ec
LAZA-GLORIA <gloria.besamba@gmail.com>
2022-06-05 09:52:09 +0200

    first step

---
+++ js/script.js
@@ -0,0 +1,6 @@
+import { addToCart, totalPrice as tp, tq } from "./shoppingCart.js";
+
+console.log('importing module');
+addToCart('pain', 4);
+
+console.log(tp, tq);
---
+++ js/shoppingCart.js
@@ -0,0 +1,14 @@
+console.log("exporting module");
+
+const shippingCost= 10;
+const cart = [];
+
+export const addToCart = function(product, quantity) {
+    cart.push({product, quantity});
+    console.log(`${quantity} ${product} added to cart`);
+}
+
+const totalPrice = 20;
+const totalQuantity = 4;
+
+export { totalPrice, totalQuantity as tq};
