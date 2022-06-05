c1599fa473569e304591c4d7b9c4c31e512d4fba
LAZA-GLORIA <gloria.besamba@gmail.com>
2022-06-05 15:38:01 +0200

    module pattern with async function and immediately invoked function expression

--- js/script.js
+++ js/script.js
@@ -4,3 +4,49 @@ console.log('importing module');
 addToCart('pain', 4);
 
 console.log(tp, tq);
+
+//Une fonction asynchrone renvoie une promesse
+
+const getLastPost = async function () {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+    console.log(data);
+    console.log(data.at(-1).title);
+    console.log(data.at(-1).id);
+    console.log(data[0].title);
+    return { title: data.at(-1).title,
+            text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+//IIFE (Immediately Invoked Function Expression) (Expression de fonction invoquée immédiatement)
+//ne sera exécutée qu'une seule fois et enverra des données qu'une seule fois
+const shoppingCart2 = (function () {
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 250;
+    const totalQuantity = 13;
+
+    const addToCart = function(product, quantity) {
+        cart.push({product, quantity});
+        console.log(`${quantity} ${product} added to cart`);
+    };
+
+    const orderStock = function(product, quantity) {
+        console.log(`${quantity} ${product} order from supplier`);
+    };
+
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity
+    }
+
+})();
+
+shoppingCart2.addToCart('apples', 4);
+shoppingCart2.addToCart('pizza', 1);
+
